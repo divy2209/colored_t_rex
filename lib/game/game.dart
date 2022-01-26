2b69660b5d7a3c4e64626ec6df831b4f28c5212d
Divyansh Maheshwari <divyansh2209@gmail.com>
2022-01-26 21:35:23 +0530

    Game over and game
    Game Over and it's configurations
    Bringing the whole game together
    App icon and naming

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,23 +1,71 @@
 import 'dart:ui' as ui;
+import 'package:colored_t_rex/game/collision/collision_utils.dart';
+import 'package:colored_t_rex/game/t_rex/t_rex.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
+import 'package:flutter/gestures.dart';
 
 import 'game_config.dart';
+import 'game_over/config.dart';
+import 'game_over/game_over.dart';
+import 'horizon/horizon.dart';
+import 'obstacle/obstacle.dart';
+
+class Bg extends Component with HasGameRef {
+  Vector2 size = Vector2.zero();
+  
+  late final ui.Paint _paint = ui.Paint()..color = const ui.Color(0xffffffff);
+
+  @override
+  void render(ui.Canvas c) {
+    final rect = ui.Rect.fromLTWH(0, 0, size.x, size.y);
+    c.drawRect(rect, _paint);
+  }
+
+  @override
+  void onGameResize(Vector2 gameSize){
+    size = gameSize;
+  }
+}
+
+enum TRexGameStatus {playing, waiting, gameOver}
 
 class TRexGame extends BaseGame with TapDetector{
   TRexGame({
     required this.spriteImage
   }) : super();
-  final ui.Image spriteImage;
   
   late final config = GameConfig();
   
   @override
   ui.Color backgroundColor() => const ui.Color(0xFFFFFFFF);
 
-  late final tRex = Trex();
+  final ui.Image spriteImage;
+
+  late final tRex = TRex();
   late final horizon = Horizon();
   late final gameOverPanel = GameOverPanel(spriteImage, GameOverConfig());
+
+  @override
+  Future<void> onLoad() async {
+    add(Bg());
+    add(horizon);
+    add(tRex);
+    add(gameOverPanel);
+  }
+
+  late TRexGameStatus status = TRexGameStatus.waiting;
+  late double currentSpeed = 0.0;
+  late double timePlaying = 0.0;
+
+  bool get playing => status==TRexGameStatus.playing;
+  bool get gameOver => status==TRexGameStatus.gameOver;
+
+  @override
+  void onTapDown(TapDownDetails details){
+    onAction();
+  }
 
   void onAction() {
     if(gameOver){
@@ -25,5 +73,56 @@ class TRexGame extends BaseGame with TapDetector{
       return;
     }
     tRex.startJump(currentSpeed);
+  }
+
+  void startGame() {
+    tRex.status = TRexStatus.running;
+    status = TRexGameStatus.playing;
+    tRex.hasPlayedIntro = true;
+    currentSpeed = config.speed;
+  }
+
+  void doGameOver() {
+    gameOverPanel.visible = true;
+    status = TRexGameStatus.gameOver;
+    tRex.status = TRexStatus.crashed;
+    currentSpeed = 0.0;
+  }
+
+  void restart() {
+    status = TRexGameStatus.playing;
+    tRex.reset();
+    horizon.reset();
+    currentSpeed = config.speed;
+    gameOverPanel.visible = false;
+    timePlaying = 0.0;
+  }
+
+  @override
+  void update(double dt){
+    super.update(dt);
+
+    if(gameOver) {
+      return;
+    }
+
+    if(tRex.playingIntro && tRex.x >= tRex.config.startXPos) {
+      startGame();
+    } else if(tRex.playingIntro){}
+
+    if(playing) {
+      timePlaying += dt;
+
+      final obstacles = horizon.horizonLine.obstacleManager.children;
+      final hasCollision = obstacles.isNotEmpty &&
+          checkForCollision(obstacles.first as Obstacle, tRex);
+      if(!hasCollision) {
+        if(currentSpeed < config.maxSpeed) {
+          currentSpeed += config.acceleration;
+        }
+      } else {
+        doGameOver();
+      }
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:colored_t_rex/game_wrapper.dart';
+import 'package:flame/flame.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  //await Flame.device.fullScreen();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+
   runApp(
     const MaterialApp(
       title: "ColouredTRexGame",
